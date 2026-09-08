ba5ad9646e4a5b3a1aff29821648b901475f6fff
Mouad Benali <46760684+MouadBNL@users.noreply.github.com>
2026-09-08 13:13:43 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        int i = 0, j = n-1;
+        
+        while(i < j) {
+            while(i < j && !isAlphaNum(s[i])) i++;
+            while(i < j && !isAlphaNum(s[j])) j--;
+            cout << s[i] << " / " << s[j] << ";\n";
+            if(tolower(s[i]) != tolower(s[j])) return false;
+            i++; j--;
+        }
+
+        return true;
+    }
+
+
+    bool isAlphaNum(char c) {
+        return (c >= '0' && c <= '9') 
+            || (c >= 'a' && c <= 'z')
+            || (c >= 'A' && c <= 'Z');
+    }
+};
